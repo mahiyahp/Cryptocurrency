e21d3cf3b799ad555287d418fdfd387b444941d3
Mahiya Hoseinpour <mahiyahp3@gmail.com>
2023-09-24 17:17:18 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// npm i | npm start
+import './App.css';
+
+// Components
+import Landing from './components/Landing';
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
